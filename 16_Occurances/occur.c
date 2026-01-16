bfa81e69b4e0c1ea5992d8d7ff0958fcdb0ffea3
gokulchise <gokulragav333@gmail.com>
2026-01-17 05:13:26 +0530

    get occurances in array

---
+++ 16_Occurances/occur.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int Occurances(int array[], int length, int key);                  //Function prototype of Occurances()..
+
+int main(void)
+{
+    int myarray[50];
+    int to_find = 0;
+    size_t len  = 0;
+
+    //Prompt user for input:
+    printf("\nTo find value : ");
+    scanf("%d",&to_find);
+
+    printf("\narray len(<50): ");
+    scanf("%d",&len);
+
+    for(int i=0; i<len; i++)
+    {
+        printf("\nmyarray[%d] = ",i);
+        scanf("%d",&myarray[i]);
+    }
+
+    int occur   = Occurances(myarray, len, to_find);            //Call and get the count of occurances..
+
+    printf("\n# of %ds found is %d\n",to_find,occur); //Display the occurances on the console window..
+
+    return (0);                                                 //Indicate successful exeuction..
+}
+
+int Occurances(int array[], int length, int key)                //Function definition of occurances.
+{
+    int count = 0;
+
+    for(int i=0; i<length; i++)                                 //Iterate the loop upto length of array.
+        if(array[i] == key)  count++;                           //If key/to_find found -> increment count variable..
+
+    return (count);                                             //Return the count..
+}
