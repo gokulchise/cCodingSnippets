37dff2ed2e85ccdd84d4e4e9e2fcf470890833d2
gokulchise <gokulragav333@gmail.com>
2026-01-18 07:15:44 +0530

    flip from lowerCase to upperCase

--- 25_flipUpperCase/upper.c
+++ 25_flipUpperCase/upper.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <ctype.h>
 
-void flip_upper(char* str);                         //Function prototype of flip_upper()..
+void flip_upper(char* str);                  //Function prototype of flip_upper()..
 
 int main(void)
 {
@@ -12,21 +12,21 @@ int main(void)
     printf("\nInput lowerCase string: ");
     fgets(s, sizeof(s), stdin);
 
-    flip_upper(s);                    //Call by reference the flip_upper()..
+    flip_upper(s);                          //Call by reference the flip_upper()..
 
-    printf("\nUpper case : %s", s);      //Output the value..
+    printf("\nUpper case : %s", s);         //Output the value..
 
-    return(0);                          //Indicate successful exeuction..
+    return(0);                              //Indicate successful exeuction..
 }
 
-void flip_upper(char* str)              //Function definition of flip_upper()..
+void flip_upper(char* str)                  //Function definition of flip_upper()..
 {
     int len = 0;
     len = strlen(str);
 
     for (int i = 0; i < len; i++)
     {
-        if (islower(str[i]))             //check if lower case letter..
-            str[i] = toupper(str[i]);     //Flip into upper case letter..
+        if (islower(str[i]))                //check if lower case letter..
+            str[i] = toupper(str[i]);       //Flip into upper case letter..
     }
 }
