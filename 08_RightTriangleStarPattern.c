d61bcb8c6b284864211a2c388410969c984f442d
go-cool <gokulragav333@gmail.com>
2026-09-08 07:05:29 +0530

    right triangle star pattern
    C program to print right triangle star pattern

---
+++ 08_RightTriangleStarPattern.c
@@ -0,0 +1,32 @@
+/*!< C programme to print right triangle star pattern */
+
+/*
+
+Enter the pattern limit: 10 [ n=10 ]
+
+		c=1	c=2	c=3	c=4	c=5	c=6 c=7	c=8	c=9	10
+r=1		*
+
+r=2		*	*
+
+r=3		*	*	*
+
+r=4		*	*	*	*
+
+r=5		*	*	*	*	*
+
+r=6		*	*	*	*	*	*
+
+r=7		*	*	*	*	*	*	*
+
+r=8		*	*	*	*	*	*	*	*
+
+r=9		*	*	*	*	*	*	*	*	*
+
+r=10	*	*	*	*	*	*	*	*	*	*
+
+
+fd
+
+
+*/
