03053faea0e51e43264decb141f40b423cea7fa5
go-cool <gokulragav333@gmail.com>
2026-09-08 07:07:58 +0530

    hollow mirror right triangle star pattern
     C program to print hollow mirror right triangle star pattern

---
+++ 11_HollowMirrorRightTriangle.c
@@ -0,0 +1,50 @@
+/*!< C programme to print hollow mirror right triangle star pattern */
+
+/*
+Enter the pattern limit: 5 [ n=5 ]
+
+	c=1	c=2	c=3	c=4	c=5
+r=1					*
+
+r=2				*	*
+
+r=3			*		*
+
+r=4		*			*
+
+r=5	*	*	*	*	*
+
+
+
+*/
+
+#include <stdio.h>
+
+#define IS_HOLLOW_MIRROR_RTRIANGLE(_r,_c,_n)	((_r == _n) || (_c == _n) || (_c == ((_n-_r)+1)))
+
+#define PRINT_STAR()							((printf("* ")))
+#define PRINT_SPACE()							((printf("  ")))
+#define MOVE_NEW_LINE()							((printf("\n")))
+
+int main(void) {
+	
+	int row, col, limit = 0;
+	
+	/*!< Prompt user from input */
+	printf("\nInput a pattern limit: ");
+	scanf("%d", &limit);
+	
+	for (row = 1; row <= limit; row ++) {
+		
+		for (col = 1; col <= limit; col ++) {
+			
+			(IS_HOLLOW_MIRROR_RTRIANGLE(row, col, limit)) ? PRINT_STAR() : PRINT_SPACE();
+		}
+		
+		MOVE_NEW_LINE();
+		
+	}
+	
+	
+	return (0);					/*!< Indicate successful execution.. */
+}
