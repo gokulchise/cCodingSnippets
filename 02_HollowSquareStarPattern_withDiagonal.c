e9abf50e60476c18e75e3ef8a35b192142524584
go-cool <gokulragav333@gmail.com>
2026-09-08 06:51:55 +0530

    hollow square star pattern with diagonal
    C program to print hollow square star pattern with diagonal

---
+++ 02_HollowSquareStarPattern_withDiagonal.c
@@ -0,0 +1,51 @@
+/*!<C program to print hollow square star pattern with diagonal */
+
+/*
+ n=5 [Pattern limit]
+ 
+	c=1 c=2	c=3	c=4	c=5
+r=1	*	*	*	*	*
+	
+r=2	*	*		*	*
+	
+r=3	*		*		*
+	
+r=4	*	*		*	*
+	
+r=5	*	*	*	*	*
+
+Logic:
+	1. Hollow square:	print '*' -> ((r == 1) || (c == 1) || (r == n) || (c == n))
+	2. Left diagonal:   print '*' -> (r == c) 										=> [r=2, c=2], [r=3, c=3], [r=4, c=4]
+	3. Right diagonal:	print '*' -> (c == ((n-r)+1))								=> [c=4, r=2], [c=2, r=4]
+
+*/
+
+#include <stdio.h>
+
+#define IS_HOLLOW_SQUARE(_r, _c, _n)			((_r == 1) || (_c == 1) || (_r == _n) || (_c == _n))
+#define IS_LEFT_DIAGONAL(_r, _c, _n)			(_r == _c)
+#define IS_RIGHT_DIAGONAL(_r, _c, _n)			(_c == ((_n-_r)+1))
+#define IS_HOLLOW_DIAGONAL_SQUARE(_r, _c, _n)	(IS_HOLLOW_SQUARE(_r, _c, _n) || IS_LEFT_DIAGONAL(_r, _c, _n) || \
+												 IS_RIGHT_DIAGONAL(_r, _c, _n))
+												 
+												 
+int main(void) {
+	
+	int row, col, limit = 0;
+	
+	/*!<Prompt user data*/
+	printf("\nInput a pattern limit: ");
+	scanf("%d", &limit);
+	
+	for (row = 1; row <= limit; row ++) {
+		
+		for (col = 1; col <= limit; col ++) {
+			
+			IS_HOLLOW_DIAGONAL_SQUARE(row, col, limit) ? printf("* ") : printf("  ");
+		}
+		printf("\n");
+	}
+	
+	return (0);			/*!<Indicate successful execution..*/
+}
